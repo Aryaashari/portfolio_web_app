06a5302254fcacf772088370499e45746edd7e45
Arya <aryaashari100@gmail.com>
2022-05-15 00:35:57 +0700

    create column for profiles table

--- database/migrations/2022_05_14_173232_create_profiles_table.php
+++ database/migrations/2022_05_14_173232_create_profiles_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->text('profile_description');
+            $table->string('profile_picture');
+            $table->string('cv_file');
         });
     }
 
