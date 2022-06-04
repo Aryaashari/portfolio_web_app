a18d8c967274edb5a063ebdd437f4b2498d1fe53
Arya <aryaashari100@gmail.com>
2022-06-05 03:36:01 +0700

    comment some code in web route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\BlogController;
-use App\Http\Controllers\PageController;
+// use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,11 +26,11 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 
 // Projects
-Route::get('/projects', [PageController::class, 'projects']);
+// Route::get('/projects', [PageController::class, 'projects']);
 
 // Blog
-Route::get('/blog', [PageController::class, 'blog']);
+// Route::get('/blog', [PageController::class, 'blog']);
 
-Route::get('/single-post', [BlogController::class, 'singlePost']);
+// Route::get('/single-post', [BlogController::class, 'singlePost']);
 
-Route::get('/post-category', [BlogController::class, 'postCategory']);
+// Route::get('/post-category', [BlogController::class, 'postCategory']);
