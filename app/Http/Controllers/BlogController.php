279871ac9ebfdea8ae0f508b78ceb0f66dc30c87
Arya <aryaashari100@gmail.com>
2022-06-05 04:55:46 +0700

    make blog page to dynamic

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Page;
+use App\Models\Post;
 
 class BlogController extends Controller
 {
     public function index() {
         $blogPage = Page::where('page_name','blog')->get()->first();
-        return view('frontend.blog', ["blogPage" => $blogPage]);
+        $posts = Post::all();
+        
+        return view('frontend.blog', ["blogPage" => $blogPage, "posts" => $posts]);
     }
 }
--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -39,17 +39,20 @@
             <div class="row">
                 <div class="col-md-6 col-xl-4 col-12">
 
+                    @foreach ($posts as $post)
+                        
+                        <div class="post mt-4">
+                            <div class="post_thumb" style="background-image: url({{ asset('storage/posts/'.$post->post_background) }});">
+                                <div class="post_category">{{ $post->category->category }}</div>
+                            </div>
+                            <div class="post_text">
+                                <a href="{{ url('/blog/post/'.$post->slug) }}" class="post_title">{{ $post->title }}</a>
+                                <p>{{ $post->author }} - {{ $post->created_at->diffForHumans() }}</p>
+                                <a href="{{ url('/blog/post/'.$post->slug) }}" class="ut_btn_primary btn">Read</a>
+                            </div>
+                        </div>
 
-                    <div class="post mt-4">
-                        <div class="post_thumb" style="background-image: url({{ asset('frontend/assets/img/project_1.jpg') }});">
-                            <div class="post_category">Website</div>
-                        </div>
-                        <div class="post_text">
-                            <a href="{{ url('/single-post') }}" class="post_title">Pengenalan dasar website dengan menggunakan HTML, CSS, JS</a>
-                            <p>Admin - 2 hours ago</p>
-                            <a href="{{ url('/single-post') }}" class="ut_btn_primary btn">Read</a>
-                        </div>
-                    </div>
+                    @endforeach
 
 
                 </div>
