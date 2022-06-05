a90be9977369b1b1b745ad4e86e3f7d24f155b75
Arya <aryaashari100@gmail.com>
2022-06-05 08:42:13 +0700

    create main layout for admin

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\BlogController;
-// use App\Http\Controllers\PageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 // Home
 Route::get('/', [HomeController::class, 'index']);
@@ -33,6 +22,11 @@ Route::get('/blog', [BlogController::class, 'index']);
 Route::get('/blog/post/{post:slug}', [BlogController::class, 'detail']);
 Route::get('/blog/category/{category:category}', [BlogController::class, 'category']);
 
-// Route::get('/single-post', [BlogController::class, 'singlePost']);
 
-// Route::get('/post-category', [BlogController::class, 'postCategory']);
+// Admin
+Route::prefix('admin')->group(function() {
+
+    // Dashboard
+    Route::view('/dashboard', 'admin.layouts.main');
+
+});
